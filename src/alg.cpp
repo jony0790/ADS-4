400e5d2a5a04787d724b3e5bc8b25aef77a4e4c1
Джалоладдин <joniatabekovich07@gmail.com>
2026-04-22 22:48:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,72 @@
-// Copyright 2021 NNTU-CS
+#include "alg.h"
+
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            }
+        }
+    }
+
+    return count;
 }
+
+
+// 2. Два указателя (O(n))
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    int left = 0;
+    int right = len - 1;
+
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+
+        if (sum == value) {
+            count++;
+            left++;
+            right--;
+        }
+        else if (sum < value) {
+            left++;
+        }
+        else {
+            right--;
+        }
+    }
+
+    return count;
 }
+
+
+int binarySearch(int *arr, int left, int right, int target) {
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        if (arr[mid] == target)
+            return 1;
+        else if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return 0;
+}
+
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+
+    for (int i = 0; i < len; i++) {
+        int need = value - arr[i];
+
+        if (binarySearch(arr, i + 1, len - 1, need)) {
+            count++;
+        }
+    }
+
+    return count;
 }
