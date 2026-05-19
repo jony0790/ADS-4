bc09e646f49ee72f60e00c126888226f4e6f22b8
Джалоладдин <joniatabekovich07@gmail.com>
2026-05-20 00:42:03 +0300

    alg.h

---
+++ src/alg.h
@@ -0,0 +1,14 @@
+#ifndef SRC_ALG_H_
+#define SRC_ALG_H_
+#include "alg.h"
+int countPairs1(int *arr, int len, int value);
+
+int countPairs2(int *arr, int len, int value);
+
+int countPairs3(int *arr, int len, int value);
+
+git add .
+git commit -m "fix cpplint errors"
+git push
+
+#endif  // SRC_ALG_H_
